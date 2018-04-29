e63d5e8906ba49dc021b079247047d71b14d308a
avishag <avishag.csillag@fiverr.com>
2018-04-29 12:54:29 +0300

    looks ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,10 @@
 class User < ApplicationRecord
-	attr_accessor :name, :email
-
-  def initialize(attributes = {})
-    @name  = attributes[:name]
-    @email = attributes[:email]
-  end
-
-  def formatted_email
-    "#{@name} <#{@email}>"
-  end
-
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
-  					format: { with: VALID_EMAIL_REGEX }
+  					format: { with: VALID_EMAIL_REGEX },
+  					uniqueness: { case_sensitive: false }
 
 end
