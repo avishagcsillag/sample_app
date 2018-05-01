c15b3666f5d1cb7fd43d48fd6e63c5cace3012ab
avishag <avishag.csillag@fiverr.com>
2018-05-01 16:52:17 +0300

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
 
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
   def new
   	@user = User.new
@@ -36,6 +41,22 @@ class UsersController < ApplicationController
     end
   end
 
+  # Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def destroy
+    @name = User.find(params[:id]).name
+    User.find(params[:id]).destroy
+    flash[:success] = "The user: "
+    flash[:success] << @name
+    flash[:success] << " was deleted successfully"
+    redirect_to users_url
+  end
+
+############################################ private methods ################################################
   private
 
     def user_params
@@ -54,10 +75,9 @@ class UsersController < ApplicationController
       end
     end
 
-    # Confirms the correct user.
-    def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user?(@user)
+    # Confirms an admin user.
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 
 
