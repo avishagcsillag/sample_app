8cb04a42c38a00d5bd2a912c60921d939c08c54e
avishag <avishag.csillag@fiverr.com>
2018-05-01 07:02:45 +0300

    all tests ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   
+  attr_accessor :remember_token
+
 before_save { email.downcase! }
 
   validates :name, presence: true, length: { maximum: 50 }
@@ -20,5 +22,16 @@ before_save { email.downcase! }
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+
+  # Returns a random token.
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+  	self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
 
 end
